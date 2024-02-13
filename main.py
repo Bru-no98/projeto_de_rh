0ea68de6bda3e14374c65c93040bd5c47c618ab5
Bruno Kaue Garcia <bkgarcia98@gmail.com>
2024-02-13 19:49:23 -0300

    Ajustar código para arquivo json

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from contratacao import Funcionario
 from folha_de_pagamento import FolhaPagamento
 
-funcionario = Funcionario('bruno', 'salva vidas', 3000)
+funcionario = Funcionario('Bruno Garcia', 'Analista de Vendas', 3000)
 funcionario.gerar_olerite()
 funcionario.gerar_olerite()
 funcionario.gerar_olerite()
